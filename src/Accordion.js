932d2c6af30e0fb519626548bc30a0310a1bf855
왕준수 <wnstn819@douzone.com>
2022-02-18 14:30:38 +0900

    first-commit

---
+++ src/Accordion.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+import React from 'react';
+
+const Accordion = () => {
+    return(
+        <View>
+            <Text>dfasfadsfadsfdsa</Text>
+        </View>
+    )
+}
+
+export default Accordion;
